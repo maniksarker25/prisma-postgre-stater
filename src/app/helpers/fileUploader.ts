48ddd4273225a82cb3e9e10441bd78139d44a486
manik <maniksarker652@gmail.com>
2025-07-15 10:38:28 +0600

    work on local file uploader and s3 uploader

--- src/app/helpers/fileUploader.ts
+++ src/app/helpers/fileUploader.ts
@@ -1,61 +1,106 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { Request } from "express";
 import multer from "multer";
-import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
 import fs from "fs";
-import config from "../config";
-cloudinary.config({
-  cloud_name: "dp6nuvot3",
-  api_key: "578152791375281",
-  api_secret: "N8x2HVDeXXZvOUJkAV40S-nWx-g",
-});
+export const uploadFile = () => {
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      let uploadPath = "";
 
-const uploadImageToCloudinary = async (
-  imageName: string,
-  path: string
-): Promise<Record<string, unknown>> => {
-  // console.log("from image uploader", imageName, path);
-  //* make by me-------------
-  return new Promise((resolve, reject) => {
-    cloudinary.uploader.upload(
-      path,
-      { public_id: imageName },
-      function (error, result) {
-        if (error) {
-          reject(error);
-        }
-        resolve(result as UploadApiResponse);
-        fs.unlink(path, (err) => {
-          if (err) {
-            reject(err);
-          } else {
-            console.log("file is deleted successfully");
-          }
-        });
+      if (file.fieldname === "profile_image") {
+        uploadPath = "uploads/images/profile";
+      } else if (file.fieldname === "category_image") {
+        uploadPath = "uploads/images/category";
+      } else if (file.fieldname === "league_image") {
+        uploadPath = "uploads/images/league_image";
+      } else if (file.fieldname === "team_logo") {
+        uploadPath = "uploads/images/team_logo";
+      } else if (file.fieldname === "team_bg_image") {
+        uploadPath = "uploads/images/team_bg_image";
+      } else if (file.fieldname === "player_image") {
+        uploadPath = "uploads/images/player_image";
+      } else if (file.fieldname === "player_bg_image") {
+        uploadPath = "uploads/images/player_bg_image";
+      } else if (file.fieldname === "reward_image") {
+        uploadPath = "uploads/images/reward_image";
+      } else {
+        uploadPath = "uploads";
       }
-    );
+
+      if (!fs.existsSync(uploadPath)) {
+        fs.mkdirSync(uploadPath, { recursive: true });
+      }
+
+      if (
+        file.mimetype === "image/jpeg" ||
+        file.mimetype === "image/png" ||
+        file.mimetype === "image/jpg" ||
+        file.mimetype === "image/webp" ||
+        file.mimetype === "video/mp4"
+      ) {
+        cb(null, uploadPath);
+      } else {
+        //@ts-ignore
+        cb(new Error("Invalid file type"));
+      }
+    },
+    filename: function (req, file, cb) {
+      const name = Date.now() + "-" + file.originalname;
+      cb(null, name);
+    },
   });
-  //! from cloudinary
-  // cloudinary.uploader.upload(
-  //   "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  //   { public_id: "olympic_flag" },
-  //   function (error, result) {
-  //     console.log(result);
-  //   }
-  // );
-};
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, process.cwd() + "/uploads");
-  },
-  filename: function (req, file, cb) {
-    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    // cb(null, file.fieldname + "-" + uniqueSuffix);
-    cb(null, file.originalname);
-  },
-});
 
-const upload = multer({ storage: storage });
+  const fileFilter = (req: Request, file: any, cb: any) => {
+    const allowedFieldnames = [
+      "image",
+      "profile_image",
+      "league_image",
+      "category_image",
+      "team_logo",
+      "team_bg_image",
+      "player_image",
+      "player_bg_image",
+      "reward_image",
+      "video",
+    ];
 
-export const fileUploader = {
-  upload,
-  uploadImageToCloudinary,
+    if (file.fieldname === undefined) {
+      // Allow requests without any files
+      cb(null, true);
+    } else if (allowedFieldnames.includes(file.fieldname)) {
+      if (
+        file.mimetype === "image/jpeg" ||
+        file.mimetype === "image/png" ||
+        file.mimetype === "image/jpg" ||
+        file.mimetype === "image/webp" ||
+        file.mimetype === "video/mp4"
+      ) {
+        cb(null, true);
+      } else {
+        cb(new Error("Invalid file type"));
+      }
+    } else {
+      cb(new Error("Invalid fieldname"));
+    }
+  };
+
+  const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+  }).fields([
+    { name: "image", maxCount: 1 },
+    { name: "profile_image", maxCount: 1 },
+    { name: "category_image", maxCount: 1 },
+    { name: "sub_category_image", maxCount: 1 },
+    { name: "league_image", maxCount: 5 },
+    { name: "team_logo", maxCount: 1 },
+    { name: "team_bg_image", maxCount: 1 },
+    { name: "player_image", maxCount: 1 },
+    { name: "player_bg_image", maxCount: 1 },
+    { name: "reward_image", maxCount: 1 },
+    { name: "video", maxCount: 1 },
+  ]);
+
+  return upload;
 };
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import { userRoutes } from "../modules/user/user.routes";
 import { adminRoutes } from "../modules/admin/admin.routes";
 
 const router = express.Router();
